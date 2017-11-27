a9458d30aaa337e0751d0cfa40485be92ef702ce
pavel <Pavelparfenov112@gmail.com>
2017-11-27 13:46:56 -0500

    "Done"

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,30 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+    public String wuTang(int n){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("1");
+        for (int i = 2; i <= n; i++) {
+            sb.append(", ");
+            sb.append(decideOutput(i));
+        }
+        return sb.toString();
+    }
+
+    String decideOutput(int i) {
+        String output;
+        if((i % 15) == 0){
+            output = "WuTang";
+        } else if((i % 5) == 0){
+            output = "Tang";
+        } else if((i % 3) == 0){
+            output = "Wu";
+        } else {
+            output = ""+i;
+        }
+        return output;
+    }
+
 }
