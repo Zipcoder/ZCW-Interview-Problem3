df07c521ac4f1199c4970391b7878408d3b78b89
brian <brian@zipcoders-MacBook-Pro.local>
2017-11-27 13:57:39 -0500

    All done

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+    public String printNumbers1To50WuTangStyle() {
+        StringBuilder returnString = new StringBuilder();
+        for (int i = 1; i <= 50; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                returnString.append("WuTang");
+            }
+            else if (i % 3 == 0) {
+                returnString.append("Wu");
+            }
+            else if (i % 5 == 0) {
+                returnString.append("Tang");
+            }
+            else {
+                returnString.append(i);
+            }
+            if (i != 50) {
+                returnString.append(", ");
+            }
+        }
+        return returnString.toString();
+    }
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class Problem1Test {
+public class Problem3Test {
+
+    @Test
+    public void printNumbers1To50WuTangStyle() {
+
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang, 16, 17, Wu, 19, Tang, Wu, 22, 23, "
+                + "Wu, Tang, 26, Wu, 28, 29, WuTang, 31, 32, Wu, 34, Tang, Wu, 37, 38, Wu, Tang, "
+                + "41, Wu, 43, 44, WuTang, 46, 47, Wu, 49, Tang";
+
+        String actual = new Problem3().printNumbers1To50WuTangStyle();
+
+        Assert.assertEquals(expected, actual);
+
+    }
 }
