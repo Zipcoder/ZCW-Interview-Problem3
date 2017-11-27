6d83c2fffd10193cc4ea3ed383056994f3fead1b
Zachary <zstimmel@gmail.com>
2017-11-27 13:50:49 -0500

    Zach prob 3

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
 
-public class Problem1 {
+class Problem1 {
+    public String wuTang(int n) {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 3 == 0) output.append("Wu\n");
+
+            if (i % 5 == 0) output.append("Tang\n");
+
+            if (i % 3 == 0 && i % 5 == 0) output.append("WuTang\n");
+
+            else if (i % 3 != 0 && i % 5 != 0) output.append(i + "\n");
+        }
+        return output.toString();
+    }
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,95 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem3Test {
+
+    @Test
+    public void Test1(){
+        Problem1 test = new Problem1();
+
+        String expected = "1\n";
+        String actual = test.wuTang(1);
+
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void Test5(){
+        Problem1 test = new Problem1();
+        String expected = "1\n" +
+                "2\n" +
+                "Wu\n" +
+                "4\n" +
+                "Tang\n";
+        String actual = test.wuTang(5);
+
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void Test50(){
+        Problem1 test = new Problem1();
+
+        String expected = "1\n" +
+                "2\n" +
+                "Wu\n" +
+                "4\n" +
+                "Tang\n" +
+                "Wu\n" +
+                "7\n" +
+                "8\n" +
+                "Wu\n" +
+                "Tang\n" +
+                "11\n" +
+                "Wu\n" +
+                "13\n" +
+                "14\n" +
+                "Wu\n" +
+                "Tang\n" +
+                "WuTang\n" +
+                "16\n" +
+                "17\n" +
+                "Wu\n" +
+                "19\n" +
+                "Tang\n" +
+                "Wu\n" +
+                "22\n" +
+                "23\n" +
+                "Wu\n" +
+                "Tang\n" +
+                "26\n" +
+                "Wu\n" +
+                "28\n" +
+                "29\n" +
+                "Wu\n" +
+                "Tang\n" +
+                "WuTang\n" +
+                "31\n" +
+                "32\n" +
+                "Wu\n" +
+                "34\n" +
+                "Tang\n" +
+                "Wu\n" +
+                "37\n" +
+                "38\n" +
+                "Wu\n" +
+                "Tang\n" +
+                "41\n" +
+                "Wu\n" +
+                "43\n" +
+                "44\n" +
+                "Wu\n" +
+                "Tang\n" +
+                "WuTang\n" +
+                "46\n" +
+                "47\n" +
+                "Wu\n" +
+                "49\n" +
+                "Tang\n";
+        String actual = test.wuTang(50);
+
+        Assert.assertEquals(expected,actual);
+
+    }
 }
