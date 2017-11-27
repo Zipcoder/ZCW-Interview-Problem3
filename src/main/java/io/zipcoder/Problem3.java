2375fa0520d92cf562d5ca002bb30cace6e7b2bc
lina <you@example.com>
2017-11-27 14:53:31 -0500

    worked

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,37 +1,36 @@
 package io.zipcoder;
+
 import java.util.*;
 
 public class Problem3 {
-    ArrayList<String> numbers= new ArrayList<>();
-//    public void addNumbers(int n) {
+    ArrayList<String> numbers = new ArrayList<>();
+
+    //    public void addNumbers(int n) {
 //        for (int i = 0; i<n;i++){
 //            numbers.add(i+1);
 //        }
 //    }
-    public ArrayList changeValues(int n) {
+    public String changeValues(int n) {
 
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 numbers.add("WuTang");
+            } else if (i % 3 == 0) {
+                numbers.add("Wu");
+            } else if (i % 5 == 0) {
+                numbers.add("Tang");
             } else {
-                if (i % 3 == 0) {
-                    numbers.add("Wu");
-                } else {
-                    if (i % 5 == 0) {
-                        numbers.add("Tang");
-                    } else {
-                        numbers.add(Integer.toString(i));
-                    }
-                }
+                numbers.add(Integer.toString(i));
             }
         }
-        return numbers;
+        return arrayListToString(numbers);
     }
-    public String arrayListToString(ArrayList<Integer> numbers) {
+
+    public String arrayListToString(ArrayList<String> numbers) {
         StringJoiner join = new StringJoiner(", ");
-        for(Integer number : numbers) {
-            join.add(number.toString());
+        for (String number : numbers) {
+            join.add(number);
         }
         return join.toString();
-        }
+    }
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -8,11 +8,9 @@ import java.util.ArrayList;
 public class Problem3Test {
     @Test
     public void testChangeValues(){
-        ArrayList<Integer> numbers = new ArrayList<>();
         Problem3 problem = new Problem3();
-        problem.changeValues(15);
+        String actual = problem.changeValues(15);
         String expected= "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang";
-        String actual = problem.arrayListToString(numbers);
         Assert.assertEquals(expected,actual);
     }
 }
