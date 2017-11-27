b73a6f033da5f8bca85903e248579fe8db4e3f7f
jeffrey <jeffrey@zipcoders-MacBook-Pro-3.local>
2017-11-27 14:01:07 -0500

    JLawrence

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,28 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+    public static String cREAM(int chessBoxin) {
+        String wuTangForTheBabies = " ";
+        String shaolin = "";
+
+        for (int i = 1; i <= chessBoxin; i++) {
+
+            if (i % 15 == 0) {
+                wuTangForTheBabies = "WuTang";
+            } else if (i % 5 == 0) {
+                wuTangForTheBabies = "Tang";
+            } else if (i % 3 == 0) {
+                wuTangForTheBabies = "Wu";
+            } else {
+                wuTangForTheBabies = Integer.toString(i);
+            }
+            if (i == 1){
+                shaolin = shaolin + wuTangForTheBabies + ",";
+            } else {
+                shaolin = shaolin +" " + wuTangForTheBabies + ",";
+            }
+        }
+        return shaolin;
+    }
 }
+
