f800a285825b0ebf2246988a931a5e7d99d8ac7e
donald f <donaldf@zipcoders-MacBook-Pro-9.local>
2017-11-27 13:47:33 -0500

    did my best

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,31 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+
+    //The directions say print it but I returned it too to test it
+    //Maybe not good SRP but it works
+
+    public String printNumbers(int length){
+        String toReturn = "";
+        for(int i = 0; i <= length; i ++){
+            if(i%3==0 && i%15!=0){
+                System.out.print("Wu, ");
+                toReturn += "Wu, ";
+            } else if(i%5==0 && i%15!=0){
+                System.out.print("Tang, ");
+                toReturn += "Tang, ";
+            } else if(i%15==0){
+                System.out.print("WuTang, ");
+                toReturn += "WuTang, ";
+            } else {
+                System.out.print(i + ", ");
+                toReturn += i + ", ";
+            }
+        }
+        toReturn += "...";
+        System.out.println("...");
+        return toReturn;
+    }
+
 }
