bcd4fbc2177ae337ac7a419545521e61d568a51b
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-11-27 14:05:30 -0500

    need to print stringbuilder?

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+    public String wuTangTheMultiples(int n){
+        int num = n;
+        for (int i = 1; i < num; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            if(i % 3 == 0) stringBuilder.append("Wu");
+            if(i % 5 == 0) stringBuilder.append("Tang");
+
+            if(Integer.toString(i).indexOf("3") != -1) stringBuilder.append("Wu");
+            if(Integer.toString(i).indexOf("5") != -1) stringBuilder.append("Tang");
+
+            if (stringBuilder.length() == 0) System.out.print(i);
+            else System.out.print(stringBuilder);
+            System.out.println();
+        }
+        return stringBuilder.toString();
+    }
+
+    //need to figure out how to print out stringbuilder not use system.out.
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,20 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class Problem3Test {
+
+    Problem3 problem3 = new Problem3();
+
+    @Test
+    public void wuTangTheMultiples() throws Exception {
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang";
+        String actual = problem3.wuTangTheMultiples(15);
+        Assert.assertEquals(expected,actual);
+    }
+
+    //i know i should break this all up in different methods.
+    //made test go up to just 15 for sake of time. if it works to 15 it should work up to 50.
 }
