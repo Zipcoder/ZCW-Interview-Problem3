0e63c1e516605bc870f6c116eb6f249456e899e8
nehctuk <nehctuk@gmail.com>
2017-11-27 13:57:09 -0500

    Finsihed problem

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+    private StringBuilder wuTandCount = new StringBuilder();
+
+    public String wuTang(Integer n) {
+
+        for (int x = 1; x <= n; x++) {
+            if ((x % 3 != 0 || x % 5 != 0)) {
+                if (x % 3 == 0) wuTandCount.append("Wu");
+                else if (x % 5 == 0) wuTandCount.append("Tang");
+                else wuTandCount.append(x);
+            }
+            else {
+                wuTandCount.append("WuTang");
+            }
+
+            if (x != n) wuTandCount.append(", ");
+        }
+        return wuTandCount.toString();
+    }
+
 }
