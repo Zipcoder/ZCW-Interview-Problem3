94d616419bf0b89c84a0d04ef4f89ad6d2d0b9a8
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-11-27 13:44:00 -0500

    WuTang Counting>Common Core

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,35 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+    public static void main(String[] args){
+        Problem3 test = new Problem3();
+        System.out.println(test.wuTangCounting(50));
+    }
+
+    public String multPrint(int n){
+
+        String returnMe="";
+        if (n%3==0){
+            returnMe+="Wu";
+        }
+        if (n%5==0){
+            returnMe+="Tang";
+        }
+        if (n%3!=0 && n%5!=0){
+            returnMe+=""+n;
+        }
+
+        return returnMe;
+    }
+
+    public String wuTangCounting(int n){
+        StringBuilder output = new StringBuilder();
+        for (int i=1; i<n; i++){
+            output.append(multPrint(i)+", ");
+        }
+        output.append(multPrint(n));
+        return(output.toString());
+    }
+
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,32 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem3Test {
+    @Test
+    public void testMultPrint(){
+        Problem3 test=new Problem3();
+        String expected = "Wu";
+        String actual = test.multPrint(3);
+        Assert.assertEquals(expected, actual);
+
+        expected = "Tang";
+        actual = test.multPrint(5);
+        Assert.assertEquals(expected, actual);
+
+        expected = "WuTang";
+        actual = test.multPrint(15);
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testWuTangCounting(){
+        Problem3 test = new Problem3();
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang";
+        String actual = test.wuTangCounting(15);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
