5b36358101ef3fea8e410fd7b6dc35fe0e1f1990
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-11-27 14:03:17 -0500

    WuTang does not work....still figuring it out

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
-public class Problem1 {
+import java.util.ArrayList;
+
+public class Problem3 {
+
+    ArrayList<String> string1Through50 = new ArrayList<String>();
+
+    public String numbers1Through50() {
+        for (int number = 1; number <= 50; number++) {
+            if (number % 3 == 0) {
+                string1Through50.add("Wu");
+            }  else if (number % 5 == 0) {
+                string1Through50.add("Tang");
+            }  else if (number % 3 == 0 && number % 5 == 0) {
+                string1Through50.add("WuTang");
+            } else {
+                string1Through50.add(String.valueOf(number));
+            }
+        }
+        return string1Through50.toString();
+    }
 }
+
+//"WuTang condition does not want to work.....
+
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,20 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem3Test {
+
+    @Test
+    public void numbers1Through50Test() {
+        Problem3 prob3Test = new Problem3();
+        String expected = "[1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang, 16, 17, Wu, 19, Tang, Wu, 22, 23, Wu, Tang, 26, Wu, 28, 29, Wu, 31, 32, Wu, 34, Tang, Wu, 37, 38, Wu, Tang, 41, Wu, 43, 44, Wu, 46, 47, Wu, 49, Tang]";
+        String actual = prob3Test.numbers1Through50();
+        Assert.assertEquals(expected, actual);
+    }
 }
+
+
+
+
+
