86bc56e4fd3e588d1387b09d231975e8f35b3050
rikeroleg <rikeroleg@gmail.com>
2017-11-27 14:08:15 -0500

    commit

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,33 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+    //Write a java program that prints the numbers from 1 to 50 with these caveat :
+//    For multiples of 3 print “Wu” instead of the number.
+//    For multiples of 5 pring “Tang” instead of the number.
+//    For numbers which are multiples of both 3 and 5 print “WuTang”
+
+// Ex: 1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang, …
+
+    public String interator(){
+
+        String[] numbers = new String[50];
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(i);
+            if (i == 3){
+                return "Wu";
+            }
+            if(i == 5){
+                return "Tang";
+            }
+            System.out.println(i);
+// total fail ... this should be easy...
+        }
+
+        //return numbers.toString();
+        String interator = interator();
+        return interator;
+    }
+
+
+
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,17 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem3Test {
+
+    @Test
+    public void iteratorTest(){
+        Problem3 problem3 = new Problem3();
+        String example = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang";
+        String expected = example;
+        String actual = problem3.interator();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
