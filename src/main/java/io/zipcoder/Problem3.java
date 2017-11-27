3df7737a667f0a2328493d56b5e9f5aebbe9b90e
ryan <ryan@zipcoders-MacBook-Pro-5.local>
2017-11-27 14:07:51 -0500

    Ryan Maiale

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,35 @@
 package io.zipcoder;
 
-public class Problem1 {
+import java.util.ArrayList;
+
+public class Problem3 {
+
+    ArrayList<Integer> wuTangArrayList = new ArrayList<Integer>();
+    StringBuilder wuTangString = new StringBuilder();
+
+    public String wuTangMultiples(int number){
+        for (int x = 0; x <=number; x++)
+            wuTangArrayList.add(x);
+
+        for (int i = 1; i <= wuTangArrayList.size(); i++) {
+
+            if (i % 3 == 0) {
+                wuTangArrayList.remove(i);
+                wuTangString.append("Wu");
+            }
+
+            if (i % 5 == 0) {
+                wuTangArrayList.remove(i);
+                wuTangString.append("Tang");
+            }
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                wuTangArrayList.remove(i);
+                wuTangString.append("WuTang");
+            }
+            wuTangString.append(i + ", ");
+        }
+        return wuTangString.toString();
+    }
+//need to swtich to if and else if statements
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem3Test {
+    Problem3 problem3 = new Problem3();
+    int number = 15;
+
+    @Test
+    public void wuTangMultiplesTest(){
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang";
+        String actual = problem3.wuTangMultiples(number);
+        Assert.assertEquals(expected, actual);
+        //1, 2, Wu3, 4, Tang5, Wu6, 7, 8, Wu9, Tang10, 11,
+        //actual so far
+
+    }
 }
