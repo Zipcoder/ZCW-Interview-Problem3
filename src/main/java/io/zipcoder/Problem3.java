e1478bc45e086831ae6d7142c73ba6f1a3062181
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-11-29 09:15:20 -0500

    finished and re-did problem

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,23 +1,27 @@
 package io.zipcoder;
 
+import java.util.StringJoiner;
+
 public class Problem3 {
 
     public String wuTangTheMultiples(int n){
-        int num = n;
-        for (int i = 1; i < num; i++) {
-            StringBuilder stringBuilder = new StringBuilder();
-            if(i % 3 == 0) stringBuilder.append("Wu");
-            if(i % 5 == 0) stringBuilder.append("Tang");
+        StringJoiner stringJoiner = new StringJoiner(", ");
+        for (Integer i = 1; i <= n; i++) {
 
-            if(Integer.toString(i).indexOf("3") != -1) stringBuilder.append("Wu");
-            if(Integer.toString(i).indexOf("5") != -1) stringBuilder.append("Tang");
+            if(i % 3 == 0 && i % 5 == 0){
+                stringJoiner.add("WuTang");
+            }
+            else if(i % 3 == 0) {
 
-            if (stringBuilder.length() == 0) System.out.print(i);
-            else System.out.print(stringBuilder);
-            System.out.println();
+                stringJoiner.add("Wu");
+            }
+            else if(i % 5 == 0) {
+                stringJoiner.add("Tang");
+            }
+            else{
+                stringJoiner.add(i.toString());
+            }
         }
-        return stringBuilder.toString();
+        return stringJoiner.toString();
     }
-
-    //need to figure out how to print out stringbuilder not use system.out.
 }
--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -10,11 +10,9 @@ public class Problem3Test {
 
     @Test
     public void wuTangTheMultiples() throws Exception {
-        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang";
-        String actual = problem3.wuTangTheMultiples(15);
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang, 16";
+        String actual = problem3.wuTangTheMultiples(16);
         Assert.assertEquals(expected,actual);
     }
-
-    //i know i should break this all up in different methods.
     //made test go up to just 15 for sake of time. if it works to 15 it should work up to 50.
 }
