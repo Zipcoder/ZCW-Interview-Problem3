76623e2f0b899e925f40e8301de99c3a799af239
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-12-01 08:14:50 -0500

    completed kata

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -8,19 +8,16 @@ public class Problem3 {
 
     public String numbers1Through50() {
         for (int number = 1; number <= 50; number++) {
-            if (number % 3 == 0) {
+            if (number % 15 == 0) {
+                string1Through50.add("WuTang");
+            }  else if (number % 3 == 0) {
                 string1Through50.add("Wu");
             }  else if (number % 5 == 0) {
                 string1Through50.add("Tang");
-            }  else if (number % 3 == 0 && number % 5 == 0) {
-                string1Through50.add("WuTang");
-            } else {
+            }  else {
                 string1Through50.add(String.valueOf(number));
             }
         }
         return string1Through50.toString();
     }
 }
-
-//"WuTang condition does not want to work.....
-
