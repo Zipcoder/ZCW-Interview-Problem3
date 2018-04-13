27ba1d47a0e61d48dfbb21c9f3c280b92cfd727c
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-13 19:38:55 -0400

    Counter complete

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+    public String wuTangPrinter() {
+        String ret = "";
+        // For loop here
+        for (int i = 1; i <= 50; i++) {
+            ret += wuOrTang(i);
+            ret += ", ";
+        }
+        return ret.substring(0, ret.length()-2);
+    }
+
+    public String wuOrTang(int input) {
+        String ret = "";
+        if (input%3==0) ret += "Wu";
+        if (input%5==0) ret += "Tang";
+        if ((input%3!=0)&&(input%5!=0)) return Integer.toString(input);
+        return ret;
+    }
+
 }
