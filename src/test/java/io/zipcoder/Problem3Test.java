a76a8dfa94da6c4c2bb19e6a506471cdc96b9063
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-15 17:17:05 -0400

    surely there is a better way

--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem3Test {
+
+    @Test
+    public void wuTangTest(){
+        Problem3 test = new Problem3();
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, " +
+                          "Wu, 13, 14, WuTang, 16, 17, Wu, 19, Tang, Wu, 22, 23, " +
+                          "Wu, Tang, 26, Wu, 28, 29, WuTang, 31, 32, Wu, 34, Tang, " +
+                          "Wu, 37, 38, Wu, Tang, 41, Wu, 43, 44, WuTang, 46, 47, Wu, " +
+                          "49, Tang";
+        String actual = test.wuTang(50);
+        Assert.assertEquals(expected, actual);
+    }
 }
