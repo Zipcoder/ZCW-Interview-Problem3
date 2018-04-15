85d74531a6b1f911902c0d579c8069fb3190448e
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-04-15 15:01:49 -0400

    WuTangBuzz complete with TDD

--- src/main/java/io/zipcoder/Problem3.java
+++ src/main/java/io/zipcoder/Problem3.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
-public class Problem1 {
+public class Problem3 {
+
+    public String wuTangBuzz(){
+
+        String wuTangReturn = "";
+
+        for(int i = 1; i <= 50; i++){
+            if(i % 15 == 0){
+                wuTangReturn += "WuTang, ";
+            } else if(i % 3 == 0){
+                wuTangReturn += "Wu, ";
+            } else if(i % 5 == 0){
+                wuTangReturn += "Tang, ";
+            } else {
+                wuTangReturn += String.valueOf(i) + ", ";
+            }
+        }
+        // cuts off the ", " at end of String by last two substrings
+        String wuTangFinal = wuTangReturn.substring(0, wuTangReturn.length() - 2);
+        return wuTangFinal;
+    }
 }
