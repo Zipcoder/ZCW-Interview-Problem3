ad816e54d800ab9bf9fd2c517713e4147dedd7e5
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-04-15 12:07:46 -0400

    Done

--- src/test/java/io/zipcoder/Problem3Test.java
+++ src/test/java/io/zipcoder/Problem3Test.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
-public class Problem1Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem3Test {
+    @Test
+    public void testPrintNumbers(){
+        Problem3 test = new Problem3();
+        int input  = 15;
+        String expected = "1, 2, Wu, 4, Tang, Wu, 7, 8, Wu, Tang, 11, Wu, 13, 14, WuTang, ";
+        String actual = test.printNumbers(15);
+        Assert.assertEquals(expected,actual);
+    }
 }
